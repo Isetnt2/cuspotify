79515ef4a6215d2cb04b3433eb1ea8d9d804bb85
Glitch (cuspotify) <none>
2018-09-26 11:11:12 +0000

    👂🏡 Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cuspotify.github.io
 A custom spotify web client.
+
+cookies.js is for managing cookies used for the theme.
+theme-changer.js is for controlling the theme and the settings modal.
+script.js is for controlling most things on the page both back- and frontend.
+The code is dependent on JQuery and Jquery UI all required libraries are in the HTML header and some inside the body tags.
+The code is mostly not optimised for speed or reliability and is not minimized so the site may take some time to load depending on your browser and computer speed.
--- script.js
+++ script.js
@@ -99,7 +99,8 @@ var options = {
     }
 };
 $("#easyNotify").easyNotify(options);
- } 
+ }
+  
    console.log(state)
     $('#info-pic').attr('src', albumCover);
     $('#songName').text(songName);
@@ -140,12 +141,16 @@ else if (state.repeat_mode == 2){
   console.log('Changed position!');
 });
 });
-
       $('#info-pic').off('click').on('click', function(){
       window.open(('https://open.spotify.com/track/' + songId), '_blank');
       });
-
-});
+  var album1 = state.track_window.current_track.album.images[0].url;  
+  var album2 = state.track_window.next_tracks[0].album.images[0].url;
+  var album3 = state.track_window.next_tracks[1].album.images[0].url;
+    $('#album3').attr('src', album1);
+    $('#album2').attr('src', album2);
+    $('#album1').attr('src', album3);
+  });
 
 
   // Ready
@@ -157,13 +162,10 @@ else if (state.repeat_mode == 2){
     deviceId
     
   });
-$('.connect').click(function(){
-  // Connect to the player!
-  player.connect().then(success => {
+    player.connect().then(success => {
   if (success) {
     console.log('The Web Playback SDK successfully connected to Spotify!');
   }
-})
   $('.play').show();
   $('.crl').show();
   $('#info-pic').show();
@@ -190,6 +192,7 @@ $('.disconnect').click(function(){
   $('.volume').hide();
   $('.mute').hide();
   $('.previus').hide();
+  $('.connect').show();
 });
 $('.play').click(function(){
 	player.resume().then(() => { console.log('Resumed playback!'); 
@@ -295,8 +298,10 @@ $('.volume-low').show();
  $('.mute').hide(); 
  }
 }
+
+$('.playlist').click(function(){
   
-  
+  });  
 $('.repeat-track').click(function(){
 var settings = {
   "async": true,
@@ -354,6 +359,123 @@ $.ajax(settings).done(function (response) {
   $(".repeat-2").hide();
 $(".repeat-track").show();
 });
+  // a key map of allowed keys
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+// the 'official' Konami Code sequence
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length) {
+      activateCheats();
+      konamiCodePosition = 0;
+    }
+  } else {
+    konamiCodePosition = 0;
+  }
+});
+
+function activateCheats() {
+  alert('cheats activated!');
+  var canvas = document.body.appendChild( document.createElement( 'canvas' ) ),
+    context = canvas.getContext( '2d' );
+context.globalCompositeOperation = 'lighter';
+canvas.width = window.screen.availWidth;
+canvas.height = window.screen.availHeight;
+canvas.style.position = 'fixed';
+canvas.style.top = '0px';
+canvas.style.zIndex = '-100';
+draw();
+
+var textStrip = ['诶', '比', '西', '迪', '伊', '吉', '艾', '杰', '开', '哦', '屁', '提', '维'];
+
+var stripCount = 60, stripX = new Array(), stripY = new Array(), dY = new Array(), stripFontSize = new Array();
+
+for (var i = 0; i < stripCount; i++) {
+    stripX[i] = Math.floor(Math.random()*1265);
+    stripY[i] = -100;
+    dY[i] = Math.floor(Math.random()*7)+3;
+    stripFontSize[i] = Math.floor(Math.random()*16)+8;
+}
+
+var theColors = ['#cefbe4', '#81ec72', '#5cd646', '#54d13c', '#4ccc32', '#43c728'];
+
+var elem, context, timer;
+
+function drawStrip(x, y) {
+    for (var k = 0; k <= 20; k++) {
+        var randChar = textStrip[Math.floor(Math.random()*textStrip.length)];
+        if (context.fillText) {
+            switch (k) {
+            case 0:
+                context.fillStyle = theColors[0]; break;
+            case 1:
+                context.fillStyle = theColors[1]; break;
+            case 3:
+                context.fillStyle = theColors[2]; break;
+            case 7:
+                context.fillStyle = theColors[3]; break;
+            case 13:
+                context.fillStyle = theColors[4]; break;
+            case 17:
+                context.fillStyle = theColors[5]; break;
+            }
+            context.fillText(randChar, x, y);
+        }
+        y -= stripFontSize[k];
+    }
+}
+
+function draw() {
+    // clear the canvas and set the properties
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.shadowOffsetX = context.shadowOffsetY = 0;
+    context.shadowBlur = 8;
+    context.shadowColor = '#94f475';
+    
+    for (var j = 0; j < stripCount; j++) {
+        context.font = stripFontSize[j]+'px MatrixCode';
+        context.textBaseline = 'top';
+        context.textAlign = 'center';
+        
+        if (stripY[j] > 1358) {
+            stripX[j] = Math.floor(Math.random()*canvas.width);
+            stripY[j] = -100;
+            dY[j] = Math.floor(Math.random()*7)+3;
+            stripFontSize[j] = Math.floor(Math.random()*16)+8;
+            drawStrip(stripX[j], stripY[j]);
+        } else drawStrip(stripX[j], stripY[j]);
+        
+        stripY[j] += dY[j];
+    }
+  setTimeout(draw, 70);
+}
+
+
+}
   $(window).keypress(function(e) {
   if (e.keyCode == 0 || e.keyCode == 32) {
 	
